a3a4bcfcdf4ac59f65ca2c4a21684ec2c874814e
Mahesh <gmahesha@gmail.com>
2020-09-05 18:18:58 +0530

    restful web app

---
+++ rest/src/main/java/com/websystique/springboot/RestClient.java
@@ -0,0 +1,17 @@
+package com.websystique.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.net.URI;
+import java.util.LinkedHashMap;
+import java.util.List;
+ 
+import com.websystique.springboot.model.User;
+import org.springframework.web.client.RestTemplate;
+
+
+public class RestClient {
+	
+	
+}
